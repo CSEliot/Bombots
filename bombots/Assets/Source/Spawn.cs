99c900d9bb4ae1b65526db46e02989de556b8b4d
Marc Olano <olano@umbc.edu>
2014-09-07 12:41:57 -0400

    Blob spawns from prefab

--- bombots/Assets/Source/Spawn.cs
+++ bombots/Assets/Source/Spawn.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour {
 	public float spawnRate = 1f;	// how often to spawn, in seconds
+	public GameObject baseBlob;	// base blob object
 
 	private float lastSpawn;	// time of last spawn
-	private GameObject baseBlob;	// base blob object
 
 	// Use this for initialization
 	void Start () {
 		lastSpawn = Time.time;
-		baseBlob = GameObject.Find("BlobBob");
 	}
 	
 	// Update is called once per frame
