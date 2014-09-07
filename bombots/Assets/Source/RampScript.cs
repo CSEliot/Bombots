3e4f363f2880ea2c2a8ec10b53c1438d6a498451
Marc Olano <olano@umbc.edu>
2014-09-07 10:02:08 -0400

    Simple spawn class

--- bombots/Assets/Source/RampScript.cs
+++ bombots/Assets/Source/RampScript.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class RampScript : MonoBehaviour {
 	public float targetAngle;
 
+	private GameObject laser;	// gate laser to turn on and off
+	private Vector3 pivot;		// pivot point in world space
+	private float probability;	// chance to move
+
 	// Use this for initialization
 	void Start () {
 		targetAngle = -22.5f;
+		probability = 0.5f;
+		laser = GameObject.Find ("gate/laser_004");
+		pivot = new Vector3(0,0,-1);
 	}
 	
 	// Update is called once per frame
@@ -18,10 +25,8 @@ public class RampScript : MonoBehaviour {
 
 		// change direction
 		float angleMag = Mathf.Abs (angleOffset);
-		if (angleMag < 0.001f && Random.value < 0.5f*Time.deltaTime) {
+		if (angleMag < 0.001f && Random.value < probability*Time.deltaTime) {
 			targetAngle = -targetAngle;
-
-			GameObject laser = GameObject.Find ("/gate/laser_004");
 			laser.renderer.enabled = targetAngle > 0;
 		}
 
@@ -29,7 +34,6 @@ public class RampScript : MonoBehaviour {
 		                          -angleMag, angleMag);
 
 		// rotate toward target
-		Vector3 pivot = new Vector3(0,0,-1);
 		transform.RotateAround(pivot, Vector3.right, angleOffset);
 
 	}
---
+++ bombots/Assets/Source/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+	public float spawnRate;	// how often to spawn, in seconds
+	public float lastSpawn;	// time of last spawn
+
+	// Use this for initialization
+	void Start () {
+		spawnRate = 1f;
+		lastSpawn = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float now = Time.time;
+		if (now > lastSpawn + spawnRate) {
+			lastSpawn = now;
+			Instantiate(GameObject.Find("Blob"), transform.position, Quaternion.identity);
+		}
+	}
+}
