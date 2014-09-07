76f804d3762aadb20a5a87960275e256c568f602
Paul Tschirgi <pault1@umbc.edu>
2014-09-07 16:36:26 -0400

    ultimate level

---
+++ bombots/Assets/tweenCam.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var targCam : GameObject;
+var speed : float = .7;
+
+var spotlight : GameObject;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+
+	this.transform.position = Vector3.Lerp(this.transform.position, targCam.transform.position, Time.deltaTime * speed);
+
+	this.transform.LookAt(spotlight.transform);
+	
+	speed += .1 * Time.deltaTime;
+}
